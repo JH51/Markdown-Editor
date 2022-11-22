a9132091b68ebb0e0c514ba042b4a54e8d5fbd6d
JH51 <jonhuang5@gmail.com>
2022-11-22 17:43:41 -0500

    Production mode serves app on localhost:3000

--- server/express.js
+++ server/express.js
@@ -12,7 +12,7 @@ const path = require("path");
 app.get("/", (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, "../build/bundle.html"));
 });
-app.use("/build", express.static(path.join(__dirname, "../build/bundle.html")));
+app.use("/build", express.static(path.join(__dirname, "../build")));
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
