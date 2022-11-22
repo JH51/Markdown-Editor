5a6d85ba9cad6a1a2266caf2f58555cc21840dce
JH51 <jonhuang5@gmail.com>
2022-11-22 09:58:32 -0500

    Import styles; styles dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,14 @@ module.exports = {
         loader: "babel-loader",
         options: { presets: ["@babel/env", "@babel/preset-react"] },
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
